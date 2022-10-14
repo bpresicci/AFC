ae042301b92b4e61e87c74ac1fcf683aa75460e9
bpresicci <80779795+bpresicci@users.noreply.github.com>
2022-10-14 22:25:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from retrieve_data import yahoo_finance, income_statement, balance_sheet, cash_flow
-from indicators import fun_ebit_margin
-
+from indicators_benedetta import fun_ebit_margin
 prada = {
-    "code": "PRDSY",
+    "k" : 0.833,
+    "ticker": "PRDSY",
     "path_is": r"C:\Users\bened\PycharmProjects\pythonProject1\income_statement.csv",
     "path_bs": r"C:\Users\bened\PycharmProjects\pythonProject1\balance_sheet.csv",
     "path_cf": r"C:\Users\bened\PycharmProjects\pythonProject1\cash_flow.csv"
@@ -12,8 +12,6 @@ prada = yahoo_finance(prada)
 BS = balance_sheet(prada)
 IS = income_statement(prada)
 CF = cash_flow(prada)
-
 # EVENTUALMENTE AGGIUNGI CORREZIONI QUI
-IS.loc['Ebit'] = [489484000,20061000,306779000,323846000]
-
+IS['EBIT'] = [323846000,306779000,20061000,489484000]
 ebit_margin = fun_ebit_margin(IS)
--- retrieve_data.py
+++ retrieve_data.py
@@ -1,11 +1,11 @@
-import yfinance as yf
+import yahooquery as yf
 
 def yahoo_finance(dictionary):
     """
     :param dictionary: dictionary with code of the desired company eg PRDSY
     :return: updated dictionary
     """
-    company = yf.Ticker(dictionary["code"])
+    company = yf.Ticker(dictionary["ticker"])
     dictionary["company"] = company
     return dictionary
 
@@ -14,7 +14,8 @@ def income_statement(dictionary):
     :param dictionary: dictionary with the company extracted from yahoo_finance, its code and the path
     :return: income statement of the company and income statement until 2018
     """
-    IS = dictionary["company"].financials
+    IS = yf.Ticker(dictionary["ticker"]).income_statement(frequency='a')
+    IS = IS[:-1] # TOGLIE L'ULTIMA RIGA CHE è 2022
     IS.to_csv(path_or_buf=dictionary["path_is"], na_rep="-", mode="w")
     return IS
 
@@ -23,7 +24,7 @@ def balance_sheet(dictionary):
     :param dictionary: dictionary with the company extracted from yahoo_finance, its code and the path
     :return: the balance sheet (and it saved a csv file of it)
     """
-    BS = dictionary["company"].balance_sheet
+    BS = dictionary["company"].balance_sheet(frequency = 'a')
     BS.to_csv(path_or_buf=dictionary["path_bs"], na_rep="-",mode="w")
     return BS
 
@@ -32,7 +33,8 @@ def cash_flow(dictionary):
     :param dictionary: dictionary with the company extracted from yahoo_finance, its code and the path
     :return: the cash_flow (and it saved a csv file of it)
     """
-    CF = dictionary["company"].cashflow
+    CF = dictionary["company"].cash_flow(frequency='a')
+    CF = CF[:-1]  # TOGLIE L'ULTIMA RIGA CHE è 2022
     CF.to_csv(path_or_buf=dictionary["path_cf"], na_rep="-",mode="w")
     return CF
 
